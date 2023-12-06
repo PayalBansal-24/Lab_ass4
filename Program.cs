3f7ee2eb52c9264ef3ef5b9f8db718844e8d8f83
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-12-06 19:16:49 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Ass4_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.firstName = "Payal";
+            person.lastName = "Bansal";
+            Console.WriteLine("Full Name is : " + person.fullName);
+        }
+    }
+    class Person
+    {
+        private string FirstName, LastName;
+        public string firstName
+        {
+            set
+            {
+                FirstName = value;
+            }
+        }
+        public string lastName
+        {
+            set
+            {
+                LastName = value;
+            }
+        }
+        public string fullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+    }
+}
